e06b0de2d1eed750158d386aeeb278495123a635
Gabriel ufot <gabrielufot23@gmail.com>
2020-02-21 03:07:55 +0100

    'committed'

--- griffinshop/settings.py
+++ griffinshop/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'product.apps.ProductConfig',  # new
+    'product.apps.ProductConfig',  # added personally
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'griffinshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # new
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # added personally
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
